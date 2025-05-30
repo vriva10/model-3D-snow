79e91b54d5897592ae1cbcb2a115514f124f6ccf
Valentinriva <rivavalentin10@gmail.com>
2025-05-29 22:10:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -102,5 +102,5 @@ setupLightbox("animation3", "melt3");
 setupLightbox("animation4", "melt4");
 setupLightbox("animation5", "melt5");
 setupLightbox("animation6", "melt6");
-setupLightbox("animation7", "melt7", [0, 10, 20, 30], 1);
+setupLightbox("animation7", "melt7", [0, 10, 20, 30], 0.2);
 
